31afd128bfb935d4c53131b04b9c557f1c7e9eff
Michael Pereira <michaelmaldonadopereira69@gmail.com>
2020-12-02 11:39:21 -0500

    Ejemplo07

--- ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo07.java
+++ ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo07.java
@@ -38,15 +38,16 @@ public class Ejemplo07 {
         int operacion02;
         int tabla = 10;
         String cadena01 = "";
+        String cadena02 = "";
         for (int i = 1; i <= 5; i++) {
             operacion01 = tabla + i;
             operacion02 = tabla * i;
             cadena01 = String.format("%s%d + %d = %d\n", cadena01, 
                     tabla, i, operacion01);
-            cadena01 = String.format("%s%d * %d = %d\n", cadena01, 
+            cadena02 = String.format("%s%d * %d = %d\n", cadena02, 
                     tabla, i, operacion02);
         }
         
-        System.out.printf("%s\n", cadena01);
+        System.out.printf("%s\n\n%s", cadena01, cadena02);
     }
 }
